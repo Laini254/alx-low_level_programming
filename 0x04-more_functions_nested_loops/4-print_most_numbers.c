398011bfad3dc85f9a05932ee853bc25db6b7ca7
Laini254 <emmahlaini@gmail.com>
2022-04-14 15:29:09 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	char a = 0;
+
+	while (a < 10)
+
+	{
+		if (a != 2 && a != 4)
+		{
+			_putchar('0' + a);
+		}
+
+		n++;
+	}
+	_puthar('\n');
+
+}
