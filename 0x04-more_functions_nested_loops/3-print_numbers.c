88a0d1cfe66032dabd1474680770ad8349c6f08e
Laini254 <emmahlaini@gmail.com>
2022-04-14 15:07:48 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*print_numbers - prints numbers (0 through 9)
+*ends with a new line
+*Return: (0 through 9)
+*/
+void print_numbers(void)
+{
+	return (0, 1, 2, 3, 4, 5, 6, 7, 8, 9\n);
+
+}
