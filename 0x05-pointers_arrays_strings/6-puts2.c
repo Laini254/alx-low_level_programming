2ee16fa81fd5a01917a5ea00d82bc394a4066a5f
Laini254 <emmahlaini@gmail.com>
2022-04-20 12:17:35 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *puts2 - prints every character of a string
+ *@str: pointer to the string
+ *Return: 0
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+
+		i++;
+	}
+	_putchar('\n');
+}
