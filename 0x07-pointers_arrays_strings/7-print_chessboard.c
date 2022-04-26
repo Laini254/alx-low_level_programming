046f92b240e719ccd140e8be95ee507b41800a38
Laini254 <emmahlaini@gmail.com>
2022-04-26 15:11:46 -0700

    Task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard.
+ *@a: pointer
+ *Return: 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	unsigned int c, d = 0;
+
+	for (c = 0; c < 64; c++)
+	{
+		if (c % 8 == 0 && c != 0)
+		{
+			d = c;
+			_putchar('\n');
+		}
+		_putchar(a[c / 8][c - d]);
+	}
+	_putchar('\n');
+}
