debb66da9790b7f97ad83a8e97355818d4433312
Laini254 <emmahlaini@gmail.com>
2022-05-02 22:44:20 -0700

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main: entry point
+ * @argc: number of command line arguments
+ * @argv: array that contains the program command line arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a;
+
+	for (a = 0; a < argc; a++)
+		printf("%s\n", argv[a]);
+	return (0);
+}
